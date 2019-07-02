4651bae6a05e6c1da61a52e06973fe873706591e
or-p <or-p@yit.co.il>
2019-07-02 13:00:15 +0300

    Initial Commit

---
+++ codableApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codableApp
+//
+//  Created by Or paz tal on 02/07/2019.
+//  Copyright © 2019 Or paz tal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
